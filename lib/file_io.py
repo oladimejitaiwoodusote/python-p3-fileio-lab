b826f2a93fecec70e9e3fc3c524c42f4135d55a1
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2023-04-06 17:37:58 -0400

    python files

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,12 @@
 def write_file(file_name, file_content):
-    pass
+    with open(file_name + ".txt", mode = 'w', encoding = 'utf-8') as text_file:
+        text_file.write(file_content)
 
 def append_file(file_name, append_content):
+    with open(file_name + ".txt", mode = "a", encoding= "utf-8") as text_file:
+        text_file.write(append_content)
     pass
 
 def read_file(file_name):
-    pass
+    text_file = open(file_name + ".txt", encoding="utf-8")
+    return text_file.read()
